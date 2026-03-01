114071132d8691b27e20d4b665e05a29f95e6cf9
Saikat Saha <saikats_5@yahoo.co.in>
2026-03-01 19:52:01 +0530

    DB and model setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express"
+import "dotenv/config"
+import connectDB from "./database/db.js"
 
 const app = express()
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
+	connectDB()
 	console.log(`Server is running on port ${PORT}`)
 })
